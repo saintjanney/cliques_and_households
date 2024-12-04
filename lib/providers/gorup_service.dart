5a9de4114ff695d7bcbd5dafb20ea070fa7eff11
saint1997 <allenjanney@gmail.com>
2024-12-04 17:14:18 +0000

    --

--- lib/providers/gorup_service.dart
+++ lib/providers/gorup_service.dart
@@ -1,5 +1,17 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class GorupService with ChangeNotifier {
-  createGroup() {}
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<void> createGroup() async {
+    try {
+      await _firestore.collection('groups').add({
+        'name': 'Clique',
+        'members': [],
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
 }
